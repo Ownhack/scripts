53739905eba2180fc6207c21e3693476c67fa146
Daniel Shiposha <shipdan@yandex.ru>
2018-11-29 01:10:33 +0300

    add health alert scripts

---
+++ reset_health_alert.sh
@@ -0,0 +1,1 @@
+curl --header 'Content-Type: application/json' --request POST --data '{}' http://195.19.40.201:31168/reset_health_alert
---
+++ set_health_alert.sh
@@ -0,0 +1,1 @@
+curl --header 'Content-Type: application/json' --request POST --data '{}' http://195.19.40.201:31168/set_health_alert
